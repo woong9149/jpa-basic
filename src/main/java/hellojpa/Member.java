50459dabc071dcdfe02d60c60444b9358a54a44e
woong9149 <sewoong0409@naver.com>
2022-11-24 12:26:23 +0900

    221124 Mapping_2

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,9 +4,21 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+//@SequenceGenerator(
+//        name = "MEMBER_SEQ_GENERATOR",
+//        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+//        initialValue = 1, allocationSize = 1
+//)
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ", allocationSize = 1
+)
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.TABLE,
+    generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
     @Column(name = "name", nullable = false)
     private String username;
